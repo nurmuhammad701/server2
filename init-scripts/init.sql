1fab973a8439b0fc942e71f2edca2a59575eeeaf
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-09-13 15:32:47 +0500

    all

--- init-scripts/init.sql
+++ init-scripts/init.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION postgres_fdw;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL
 );
